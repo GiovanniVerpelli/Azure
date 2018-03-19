c0e3efa981aba1629419c71da6e2b64b3a1ad3b1
GiovanniVerpelli <32671720+GiovanniVerpelli@users.noreply.github.com>
2018-03-19 13:04:26 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict'
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+var rubrica = [{ nome: "pippo", professione: "cane" }, { nome: "topolino", professione: "topo" }];
+rubrica.push({ nome: "pluto", professione: "cane" });
+console.log("Array Iniziale");
+console.log(rubrica);
+
+server.route({
+    method: 'PUT',
+    path: '/api/items/{id}',
+    handler: function (request, reply) {
+        console.log("Modifico elemento nella posizione " + request.params.id);
+        console.log("Modifico elemento %O", rubrica[request.params.id]);
+        rubrica[request.params.id].nome = encodeURIComponent(request.query.nome);
+        rubrica[request.params.id].professione = encodeURIComponent(request.query.professione);
+        console.log("Array Modificato :");
+        console.log(rubrica);
+        reply(JSON.stringify(rubrica));
+    }
+});
+server.start(function () {
+    console.log('Hapi is listening to http://localhost:3000');
+});
+
+/* METODO DEL DELETE
+server.route({
+    method: 'DELETE',
+    path: '/api/items/{id}',
+    handler: function (request, reply) {
+        console.log("Elimino elemento nella posizione " + request.params.id);
+        console.log("Elimino elemento %O", rubrica[request.params.id]);
+        rubrica.splice(request.params.id, 1);
+        console.log("Array Modificato :");
+        console.log(rubrica);
+        reply(JSON.stringify(rubrica));
+    }
+});
+server.start(function () {
+     console.log('Hapi is listening to http://localhost:3000');
+});*/
